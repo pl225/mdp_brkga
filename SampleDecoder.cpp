84da433425d396a8021aeb6d1cf66531536748a1
Matheus Abreu <matheusa596@gmail.com>
2020-03-14 09:25:02 -0300

    Fazendo decodificação por ordenação.

--- SampleDecoder.cpp
+++ SampleDecoder.cpp
@@ -33,20 +33,19 @@ SampleDecoder::SampleDecoder(string caminho) {
 // Runs in \Theta(n \log n):
 double SampleDecoder::decode(std::vector< double >& chromosome) const {
 
-	vector<int> total(this->getN()); // alocando vetor com n elementos
-	iota(total.begin(), total.end(), 0); // preenchendo vetor de 0 a n - 1
+	std::vector< std::pair< double, unsigned > > ranking(chromosome.size());
 
-	vector<pair<int, int>> selecionados; // criando vetor para os m selecionados: <pos, elemento>
+	for(unsigned i = 0; i < chromosome.size(); ++i) {
+		ranking[i] = std::pair< double, unsigned >(chromosome[i], i);
+	}
 
-	for(unsigned i = 0; i < chromosome.size(); i++) { // decodificando vetor de rand
-		double valor = total.size() * chromosome[i]; // pos do elemento selecionado
-		double teto = ceil(valor);
-		double piso = floor(valor);
-		int pos = teto - valor < 0.0001 && teto < total.size() ? teto : floor(valor);
-		//pos = pos < total.size() ? pos : total.size()-1;
-		selecionados.push_back(make_pair(pos, total[pos])); // guardando o elemento e sua posicao
+	// Here we sort 'permutation', which will then produce a permutation of [n] in pair::second:
+	std::sort(ranking.begin(), ranking.end());
 
-		total.erase(total.begin() + pos); // apagando elemento de pos
+	// permutation[i].second is in {0, ..., n - 1}; a permutation can be obtained as follows
+	std::vector< unsigned > selecionados;
+	for(int i = 0; i < this->getM(); i++) {
+		selecionados.push_back(ranking[i].second);
 	}
 
 	float fitness = 0, 
@@ -57,7 +56,7 @@ double SampleDecoder::decode(std::vector< double >& chromosome) const {
 
 	for (int i = 0; i < selecionados.size(); i++) { // calculando diversidade total e elemento menos diverso
 		for (int j = 0; j < selecionados.size(); j++) {
-			dist = this->distancias[selecionados[i].second][selecionados[j].second];
+			dist = this->distancias[selecionados[i]][selecionados[j]];
 			fitness += dist; // diversidade total
 			diversidadeAtual += dist; // diversidade de i
 		}
@@ -69,50 +68,6 @@ double SampleDecoder::decode(std::vector< double >& chromosome) const {
 	}
 
 	fitness /= 2; // removendo repetição de diversidades.
-
-	int u = -1; // substituirá o elemento menos diverso
-	
-	for (const int v : total) { // procurando primeiro elemento não escolhido v mais diverso que o menor diverso encontrado
-		for (int i = 0; i < selecionados.size(); i++) {
-			if (i == indElementoMenosDiverso) {
-				continue;
-			}
-			diversidadeAtual = diversidadeAtual + // calculando diferença de diversidade de acordo com Martí
-				(this->distancias[selecionados[i].second][v] -
-				this->distancias[selecionados[i].second][selecionados[indElementoMenosDiverso].second]);
-		}
-		if (fitness < fitness + diversidadeAtual) { // averiguando se nova configuração é mais diversa
-			u = v;
-			break;
-		} else {
-			diversidadeAtual = 0;
-		}
-	}
-
-	if (u != -1) {
-
-		int elementoMaisDiverso = u;
-		for (int i = 0; i < indElementoMenosDiverso; i++) { 
-			if (selecionados[i].second < elementoMaisDiverso) { // calculando a posição do elemento u após remoções de elementos no fase de seleção
-				u--;
-			}
-		} 
-
-		int qtdElementosAposRemocao = this->getN() - indElementoMenosDiverso; // calculando o valor do alelo do novo elemento
-		chromosome[indElementoMenosDiverso] = (double) u / qtdElementosAposRemocao;
-		int elementoMenosDiverso = selecionados[indElementoMenosDiverso].second;
-		
-		for (int a = indElementoMenosDiverso + 1; a < this->getM(); a++) {
-			int elemento = selecionados[a].second;
-			if (elemento > elementoMenosDiverso && elemento < elementoMaisDiverso) {
-				chromosome[a] = (double) (selecionados[a].first + 1) / (this->getN() - a);
-			} else if (elemento < elementoMenosDiverso && elemento > elementoMaisDiverso) {
-				chromosome[a] = (double) (selecionados[a].first - 1) / (this->getN() - a);
-			}
-		}
-
-		return fitness + diversidadeAtual;
-	}
 
 	return fitness;
 }
